1d7ab336a8f71e1a5154ba4cabf2bb12803fc8b1
Boubacar-Beuzy-Ba <bouba0178@gmail.com>
2025-01-03 11:35:34 +0000

    Resume updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,19 @@ export default function RootLayout({
           {children}
           <Analytics />
         </Providers>
+        </--Start of Tawk.to Script--> */}
+        <script type="text/javascript">
+        let Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        let s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/66388b3507f59932ab3c6f87/1ht6dvi6a';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script>
+        {/* <!--End of Tawk.to Script-->
       </body>
     </html>
   );
